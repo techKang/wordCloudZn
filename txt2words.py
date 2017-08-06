78a276789f337c5e9a2e5b687e5e8563f025bd40
kangs <ksc@mail.ustc.edu.cn>
2017-08-06 22:56:09 +0800

    what happened?!

--- txt2words.py
+++ txt2words.py
@@ -1,43 +1,40 @@
 # -*- coding:utf-8 -*-
 import re
+
 '''
 this python file is used to remove all punctuations in source txt files.
 '''
 
+
 class Txt2words():
-    def __init__(self,srcfile='src.txt',desfile='des.txt'):
-        self.srcfile=srcfile
-        self.desfile=desfile
+    def __init__(self, srcfile='src.txt', desfile='des.txt'):
+        self.srcfile = srcfile
+        self.desfile = desfile
 
     def entxt(self):
         srcname = self.srcfile
         desname = self.desfile
-        with open(srcname, 'r',encoding='utf-8') as src:
+        with open(srcname, 'r', encoding='utf-8') as src:
             srctext = src.read()
             with open(desname, 'w') as des:
-                destext = re.sub('\W',' ',srctext)
-                destext=' '.join(destext.split())
+                destext = re.sub('\W', ' ', srctext)
+                destext = ' '.join(destext.split())
                 des.write(destext)
                 pass
             pass
         pass
 
 
-if __name__=='__main__':
-    testClass=Txt2words()
+if __name__ == '__main__':
+    testClass = Txt2words()
     testClass.entxt()
-    with open(testClass.srcfile,'r',encoding='utf-8') as src:
+    with open(testClass.srcfile, 'r', encoding='utf-8') as src:
         print('source file:')
-        srctext=src.read(100)
+        srctext = src.read(300)
         print(srctext)
         pass
-    with open(testClass.desfile,'r') as des:
+    with open(testClass.desfile, 'r') as des:
         print('\ndestination file:')
-        destext=des.read(100)
+        destext = des.read(300)
         print(destext)
         pass
-
-
-    #test
-
-
