556fde0390629806fb278a5fc26a7662787241d1
kangs <ksc@mail.ustc.edu.cn>
2017-08-07 10:51:24 +0800

    ignore freq.txt and adjust code to fit pe8

--- splitChinese.py
+++ splitChinese.py
@@ -3,29 +3,29 @@
 splitChinese.py
 ~~~~~~~~~~~~~~~~~
 This python file is used to split Chinese sentences to separated words.
-Source file should be preprocessed by txt2words.py
+Source file should be processed by txt2words.py after be processed by this python file.
 '''
 
 import jieba
 
+
 class SplitChinese():
-    def __init__(self, srcfile='src.txt', desfile='des.txt'):
+    def __init__(self, srcfile='src.txt', desfile='splited.txt'):
         self.srcfile = srcfile
         self.desfile = desfile
 
     def splitChinese(self):
-        with open(self.srcfile,'r',encoding='utf-8') as src:
-            geneStr=jieba.cut(src.read())
-            destext=" ".join(geneStr)
+        with open(self.srcfile, 'r', encoding='utf-8') as src:
+            geneStr = jieba.cut(src.read())
+            destext = " ".join(geneStr)
             pass
-        with open(self.desfile,'w') as des:
+        with open(self.desfile, 'w') as des:
             des.write(destext.encode('utf-8').decode('utf-8'))
             pass
 
 
 if __name__ == '__main__':
-
-    testClass = SplitChinese(desfile='splited.txt')
+    testClass = SplitChinese()
     testClass.splitChinese()
     with open(testClass.srcfile, 'r', encoding='utf-8') as src:
         print('source file:')
@@ -37,15 +37,3 @@ if __name__ == '__main__':
         destext = des.read(300)
         print(destext)
         pass
-    # from txt2words import Txt2words
-    # preClass=Txt2words(desfile='des.txt',srcfile='splited.txt')
-    # preClass.cntxt()
-    # with open(preClass.desfile, 'r') as des:
-    #     print('\ndestination file:')
-    #     destext = des.read(300)
-    #     print(destext)
-    #     pass
-
-
-
-
--- txt2words.py
+++ txt2words.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import re
+
 # import io
 # import sys
 # sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') #改变标准输出的默认编码
@@ -26,23 +27,21 @@ class Txt2words():
             pass
         pass
 
-
     def cntxt(self):
         srcname = self.srcfile
         desname = self.desfile
         with open(srcname, 'rb') as src:
             srctext = src.read().decode('utf-8')
         with open(desname, 'w') as des:
-            destext = re.sub('[，。？！《》（）,.?!"\'“”‘’：0-9a-zA-Z]', ' ', srctext)
+            destext = re.sub('[，。？！《》（）,.?!"\'“”‘’：、…□0-9a-zA-Z]', ' ', srctext)
             destext = ' '.join(destext.split())
             des.write(destext)
             pass
         pass
 
 
-
 if __name__ == '__main__':
-    testClass = Txt2words(desfile='des.txt',srcfile='splited.txt')
+    testClass = Txt2words(srcfile='splited.txt', desfile='des.txt')
     testClass.cntxt()
     with open(testClass.srcfile, 'r', encoding='utf-8') as src:
         print('source file:')
